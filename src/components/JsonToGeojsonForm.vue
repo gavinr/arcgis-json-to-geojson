ea6aee39966a2ab3681a433e5e67823dd0a62d7f
Gavin Rehkemper <gavin@gavinr.com>
2018-08-07 23:40:58 -0500

    remove "post to gist" feature since GitHub ended this functionality :( #16

--- src/components/JsonToGeojsonForm.vue
+++ src/components/JsonToGeojsonForm.vue
@@ -15,12 +15,6 @@
       <div class="col">
         <label v-show="showResultArea">Pretty Print <input type="checkbox" v-model="prettyPrint" /></label> 
       </div>
-      <div class="col">
-        <button type="button" class="btn btn-info float-right" v-show="showResultArea && gistLink === ''" v-on:click.prevent="postToGist()">Visualize via Gist</button>
-      </div>
-      <div class="col" v-show="gistLink">
-        <a class="btn btn-success float-right" v-bind:href="gistLink" target="_blank">See map!</a>
-      </div>
     </div>
 
   </form>
@@ -28,8 +22,6 @@
 
 <script>
 import ArcgisToGeojsonUtils from '@esri/arcgis-to-geojson-utils';
-import swal from 'sweetalert/dist/sweetalert.min';
-import axios from 'axios/dist/axios.min';
 
 export default {
   name: 'JsonToGeojsonForm',
@@ -38,8 +30,7 @@ export default {
     return {
       inputJson: '',
       resultJson: '',
-      prettyPrint: true,
-      gistLink: ''
+      prettyPrint: true
     };
   },
   computed: {
@@ -72,51 +63,9 @@ export default {
 
         this.resultJson = featureCollection;
         this.$emit('geojson', featureCollection);
-        this.gistLink = '';
       } catch (e) {
         this.resultJson = 'Invalid input.';
-        this.gistLink = '';
       }
-    },
-
-    postToGist: function() {
-      swal({
-        title: "Are you sure?",
-        content: {
-          element: "span",
-          attributes: {
-            innerHTML: 'This will post your GeoJSON to a public <a href="https://help.github.com/articles/about-gists/" target="_blank">GitHub Gist</a> so you can visualize it on a map. Are you sure you want to do this?'
-          },
-        },
-        icon: "warning",
-        buttons: true,
-        dangerMode: true,
-      })
-      .then(function(doPost) {
-        if(doPost) {
-          axios.post('https://api.github.com/gists', JSON.stringify({
-            description: 'GEOJSON created by http://arcgisjson.togeojson.com',
-            public: 'true',
-            files: {
-              'arcgis-json-to-geojson.geojson': {
-                'content': JSON.stringify(this.resultJson)
-              }
-            }
-          }), {
-            headers: {
-              "User-Agent": "arcgis-json-to-geojson",
-              "Origin": "http://togeojson.com"
-            }
-          })
-          .then(function (response) {
-            this.gistLink = response.data.html_url;
-          }.bind(this))
-          .catch(function() {
-            // console.error(error);
-          });
-        }
-      }.bind(this));
-      
     }
   }
 }
